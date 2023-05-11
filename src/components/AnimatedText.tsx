36b65e2f2eef43832b30ccbf04ba38df554e3e1b
modecode22 <epiccoder00@gmail.com>
2023-05-11 22:10:02 +0100

    some changes

--- src/components/AnimatedText.tsx
+++ src/components/AnimatedText.tsx
@@ -1,9 +1,10 @@
+import Image from "next/image"
 
 
-const AnimatedText = ({children}:{children:React.ReactNode}) => {
+const AnimatedText = ({img,children}:{children:React.ReactNode, img:string}) => {
   return (
-<section className="text-center">
-  <h1 className=" bg-main100 shadow-sm shadow-main100 p-1 rounded-xl title font-black text-8xl">{children}
+<section className="text-center relative">
+  <h1 className=" text-main600 shadow-sm shadow-main100 p-1 rounded-xl title font-black text-4xl sm:text-8xl">{children}
     <div className="aurora">
       <div className="aurora__item"></div>
       <div className="aurora__item"></div>
@@ -11,6 +12,7 @@ const AnimatedText = ({children}:{children:React.ReactNode}) => {
       <div className="aurora__item"></div>
     </div>
   </h1>
+  <Image src={`/${img}`} fill alt=""  className="rounded-lg  -z-10" /> 
 </section>)
 }
 
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,16 +2,26 @@ import AnimatedText from "./AnimatedText"
 
 const Hero = () => {
   return (
-    <main className="w-full py-10 flex justify-center items-center">
-<AnimatedText >
+    <main className="w-full py-10 flex flex-col justify-center items-center gap-8">
+  <section className="flex justify-center items-center gap-2 ">
+  <AnimatedText img="back6.jpg" >
         hello 
       </AnimatedText>
-<h1 className="font-black text-8xl p-1">
+<h1 className="font-black text-4xl md:text-8xl p-1">
     I'm 
 </h1>
-      <AnimatedText >
+      <AnimatedText img="back5.jpg" >
         Moncef
       </AnimatedText>
+    </section>      
+    <section className="flex justify-center items-center gap-2 ">
+      <AnimatedText img="back7.jpg" >
+        Full-Stack
+      </AnimatedText>
+      <h1 className="font-black text-4xl sm:text-8xl p-1">
+    Developer 
+</h1>
+    </section>      
 
 
     </main>
